8a324774f01a60b8d527a0d98d003825d846b672
Nurzamal Islam <n.zaman98@gmail.com>
2025-05-14 02:10:15 +0530

    feature s3 & SQS updated

--- src/main/java/com/nur/amazon_s3_file_service/configuration/AwsClientConfig.java
+++ src/main/java/com/nur/amazon_s3_file_service/configuration/AwsClientConfig.java
@@ -3,14 +3,9 @@ package com.nur.amazon_s3_file_service.configuration;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.util.StringUtils;
-import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
-import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
-import software.amazon.awssdk.services.s3.S3ClientBuilder;
-import software.amazon.awssdk.services.sqs.SqsClient;
 
 @Configuration
 public class AwsClientConfig {
@@ -18,43 +13,11 @@ public class AwsClientConfig {
     @Value("${aws.s3.region}")
     private String region;
 
-    @Value("${aws.s3.access-key}")
-    private String accessKey;
-
-    @Value("${aws.s3.secret-key}")
-    private String secretKey;
-
     @Bean
     public S3Client s3Client() {
-        S3ClientBuilder builder = S3Client.builder().region(Region.of(region));
-
-        if (StringUtils.hasText(accessKey) && StringUtils.hasText(secretKey)) {
-            builder.credentialsProvider(
-                    StaticCredentialsProvider.create(
-                            AwsBasicCredentials.create(accessKey, secretKey)
-                    )
-            );
-        } else {
-            builder.credentialsProvider(DefaultCredentialsProvider.create());
-        }
-
-        return builder.build();
-    }
-
-    @Bean
-    public SqsClient sqsClient() {
-        return SqsClient.builder()
+        return S3Client.builder()
                 .region(Region.of(region))
-                .credentialsProvider(StaticCredentialsProvider.create(
-                        AwsBasicCredentials.create(accessKey, secretKey))).build();
+                .credentialsProvider(DefaultCredentialsProvider.create())
+                .build();
     }
-
-//    @Bean
-//    public SqsClient sqsClient() {
-//        return SqsClient.builder()
-//                .region(Region.of(region))
-//                .credentialsProvider(DefaultCredentialsProvider.create())
-//                .build();
-//    }
-
 }
